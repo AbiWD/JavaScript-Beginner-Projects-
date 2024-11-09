6e162a9b0a5366741c5137002f115d4dd3b341de
Abhilash <137391776+AbiWD@users.noreply.github.com>
2024-11-09 21:08:26 +0530

    Add files via upload

---
+++ Adding 2 Number/script.js
@@ -0,0 +1,16 @@
+// For static numbers
+
+// let a = 10;
+// let b = 5;
+
+// var c = a + b;
+// console.log("The sum of "+a+" and"+b+" is: "+c);
+
+
+
+// For dynamic numbers:>>>>>>
+
+var a = prompt(" Please enter first number:");
+var b = prompt(" Please enter first number:");
+var c = parseInt(a) + parseInt(b);
+console.log("the sum of number "+a+" and "+b+" is: "+c);
---
+++ Square Root Of A Number/script.js
@@ -0,0 +1,3 @@
+var a = prompt("Enter a number:");
+var sqrt = Math.sqrt(a);
+alert(sqrt);
