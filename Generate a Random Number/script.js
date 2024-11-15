61d3ab160faccc0a2fef43b02d15c620ee76d000
Abhilash <137391776+AbiWD@users.noreply.github.com>
2024-11-15 16:02:58 +0530

    Add files via upload

---
+++ Generate a Random Number/script.js
@@ -0,0 +1,5 @@
+// Getting a random number between a range ------>>>>
+// Math.random() * (highestNumber - lowestNumber) + lowestNumber
+
+var a = Math.floor((Math.random() * (10-1)) + 1);
+console.log("The Generated Random Number is: "+a);
